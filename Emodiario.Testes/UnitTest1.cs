2ad7bbad6e5310be18954a5257c6545d07363b97
Matheus Bernardes <matheus.moreira@premiersoft.net>
2024-07-03 22:41:11 -0300

    Refatorando estruturas de namespaces;

---
+++ Emodiario.Testes/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Emodiario.Testes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var isTrue = true;
+            Assert.Equal(true, isTrue);
+        }
+    }
+}
